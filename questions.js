1c888160a424536a1cbdf42e941f4b0b47d73000
MonazPatel <94028582+MonazPatel@users.noreply.github.com>
2023-01-23 23:53:31 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is your Partner's favorite color?",
+    answer: "Blue",
+    options: [
+      "Pink",
+      "Blue",
+      "Black",
+      "White"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Favorite Food of your Partner?",
+    answer: "Pizza",
+    options: [
+      "Paanipuri",
+      "Meggie",
+      "Sandwich",
+      "Pizza"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What did She wear on your first Video-call",
+    answer: "Black Sweater",
+    options: [
+      "Black Sweater",
+      "Blue jacket",
+      "Black tshirt",
+      "Blue sweater"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Does your partner like to communicate via phone?",
+    answer: "Video Call",
+    options: [
+      "Voice Call",
+      "Chat",
+      "Voice Notes",
+      "Video Call"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is her favorite activity",
+    answer: "Teasing You",
+    options: [
+      "Teasing You",
+      "Listning Music",
+      "Playing cards",
+      "Chatting with friends"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
